3825b16bfb7ae08070285ae57cad718d9c60f9ca
wellington <wba@cin.ufpe.br>
2023-04-13 19:04:28 -0300

    Add middleware access control

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 // app.use(cors());
