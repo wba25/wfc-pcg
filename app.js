bbd955c1ee55b16d68ff8d76c0a1396005124618
wellington <wba@cin.ufpe.br>
2023-04-13 19:00:18 -0300

    Add middleware access control

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-var cors = require('cors');
+// var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -21,13 +21,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://wfc-pcg.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-app.use(cors());
+// app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
