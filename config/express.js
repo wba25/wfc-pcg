aed217a7212d5e18ca6c7beb767b0f473612da70
wellington <wba@cin.ufpe.br>
2023-04-14 13:13:41 -0300

    Add cors

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const config     = require('config');
+var cors = require('cors');
 
 module.exports = () => {
     const app = express();
@@ -11,6 +12,7 @@ module.exports = () => {
     // MIDDLEWARES
     app.use(bodyParser.json());
     app.use("/data", express.static('data'));
+    app.use(cors());
 
     // ROTAS
     require('../api/routes/process')(app);
